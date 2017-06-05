2418a0d4c1b308a5da5135ef9ac07f25f4b7fa61
aso1501143 <1501143@st.asojuku.ac.jp>
2017-06-05 14:26:13 +0900

    データベースの作成、読み取りで開く、閉じるのテンプレ作成

--- app/src/main/java/jp/ac/asojuku/jousenb/gacha_simulator/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/gacha_simulator/MainActivity.java
@@ -1,13 +1,36 @@
 package jp.ac.asojuku.jousenb.gacha_simulator;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase sqlDB;
+    DBManager dbm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    //フォアグラウンド時に読み取り専用でデータベースをオープンする
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+        dbm = new DBManager(this);
+        sqlDB = dbm.getWritableDatabase();
+        */
+    }
+
+    /*
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sqlDB.close();
+    }
+    */
 }
