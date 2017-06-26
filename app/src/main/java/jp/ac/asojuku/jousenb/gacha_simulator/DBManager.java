093fd299cbfb8a2bc924a048766f253acad7681c
aso1501143 <1501143@st.asojuku.ac.jp>
2017-06-26 14:27:41 +0900

    完全勝利

--- app/src/main/java/jp/ac/asojuku/jousenb/gacha_simulator/DBManager.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/gacha_simulator/DBManager.java
@@ -23,8 +23,8 @@ public class DBManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS game(gid INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT,money INTEGER,stone INTEGER)");
-        db.execSQL("CREATE TABLE IF NOT EXISTS percent(gid INTEGER, rarelity TEXT, percent INTEGER,PRIMARY KEY(gid, rarelity));");
+        db.execSQL("CREATE TABLE IF NOT EXISTS game(_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT,money INTEGER,stone INTEGER)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS percent(_id INTEGER, rarelity TEXT, percent INTEGER,PRIMARY KEY(_id, rarelity));");
 
     }
 
@@ -37,22 +37,22 @@ public class DBManager extends SQLiteOpenHelper {
 
     public void insert(SQLiteDatabase db,String inputMessage){
         String sql = "INSERT INTO game(title,money,stone) VALUES(?,?,?)";
-        String sql1 = "INSERT INTO parsent(gid,rarelity,TEXT,percent) VALUES(?,?,?)";
+        String sql1 = "INSERT INTO parsent(_id,rarelity,TEXT,percent) VALUES(?,?,?)";
         //DML文、DDL文の実行するときはexecSQLメソッドを使用
         db.execSQL(sql,new String[]{inputMessage});
     }
 
     //ゲームリストを取得
     public SQLiteCursor selectGameList(SQLiteDatabase db){
-        String selectSql = "SELECT * FROM game ORDER BY gid";
+        String selectSql = "SELECT * FROM game ORDER BY _id";
         SQLiteCursor cursor = (SQLiteCursor)db.rawQuery(selectSql, null);
         return cursor;
     }
 
     //ゲームリスト削除
     public void deleteGameList(SQLiteDatabase db, int id){
-        String deleteSql = "DELETE FROM game WHERE gid = ?";
-        String deleteSql1 = "DELETE FROM percent WHERE gid = ?";
+        String deleteSql = "DELETE FROM game WHERE _id = ?";
+        String deleteSql1 = "DELETE FROM percent WHERE _id = ?";
         db.execSQL(deleteSql,new String[]{String.valueOf(id)});
     }
 }
